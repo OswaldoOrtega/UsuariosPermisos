086fd6f7a4e1f361d038741afeb7e25e8f05a816
Oswaldo Rafael Ortega Garcia <oswaldoortega680@gmail.com>
2022-10-06 08:32:47 -0500

    Se crearon las 3 tablas principales en base de datos

--- PermisosUsuarios.sql
+++ PermisosUsuarios.sql
@@ -1,1 +1,19 @@
 CREATE DATABASE Permisosusuarios;
+CREATE TABLE usuarios(
+idusuario INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(100),
+apellidop VARCHAR(100),
+apellidom VARCHAR(100),
+fechanacimiento VARCHAR(13),
+rfc VARCHAR(13));
+CREATE TABLE ModuloRefaccion(
+codigobarras VARCHAR(100) PRIMARY KEY,
+nombre VARCHAR(100),
+descripcion VARCHAR(200),
+marca VARCHAR(100));
+CREATE TABLE ModuloTaller(
+codigoherramientas VARCHAR(100) PRIMARY KEY,
+nombre VARCHAR(100),
+medida VARCHAR(100),
+marca VARCHAR(100),
+descripcion VARCHAR(200));
